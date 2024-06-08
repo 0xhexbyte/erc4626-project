1eee4687353dd2448137eb0fca4a19dbda5c3567
0xhexbyte <mrigendrasoni26@gmail.com>
2024-06-09 00:24:40 +0800

    Adding basic token

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
 contract Vault is ERC4626 {
-    constructor(IERC20 _asset) ERC4626(_asset) ERC20("Vault Hexbyte Token", "vHT")
+    constructor(IERC20 _asset) ERC4626(_asset) ERC20("Vault Hexbyte Token", "vHT");
 }
